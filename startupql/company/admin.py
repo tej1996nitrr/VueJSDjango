7444068810b0c56c4fbd4861e41ac1ac27464909
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-10-11 21:31:41 +0530

    models

--- startupql/company/admin.py
+++ startupql/company/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class CityAdmin(admin.ModelAdmin):
+    fields = ('city_name',)
+class TitleAdmin(admin.ModelAdmin):
+    fields = ('title_name',)
+class EmployeeAdmin(admin.ModelAdmin):
+    fields = ('employee_name', 'employee_city', 'employee_title',)
+
+admin.site.register(City, CityAdmin)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+
--- startupql/company/models.py
+++ startupql/company/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    city_name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.city_name
+
+class Title(models.Model):
+    title_name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.title_name
+
+class Employee(models.Model):
+    employee_name = models.CharField(max_length=255)
+    employee_city = models.ForeignKey(City, related_name='employee_city', on_delete=models.CASCADE)
+    employee_title = models.ForeignKey(Title, related_name='employee_title', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.employee_name
